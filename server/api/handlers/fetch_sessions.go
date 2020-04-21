35b88c2139b6c27bcefce6cf374ff37fd725486e
Algys Ievlev <algys.ievlev@gmail.com>
2020-04-21 10:28:43 +0300

    [DPM-108] revome unused fetch sessions paylo

--- server/api/handlers/fetch_sessions.go
+++ server/api/handlers/fetch_sessions.go
@@ -2,20 +2,10 @@ package handlers
 
 import (
 	"context"
-	"encoding/json"
 	"platform-backend/server/api/interfaces"
 )
 
-type FetchSessionsPayload struct {
-	Deposit  string `json:"sessionId"`
-}
-
 func ProcessFetchSessionsRequest(context context.Context, req *interfaces.ApiRequest) (*interfaces.WsResponse, error) {
-	var payload FetchSessionsPayload
-	if err := json.Unmarshal(req.Data.Payload, &payload); err != nil {
-		return nil, err
-	}
-
 	return &interfaces.WsResponse{
 		Type:    "response",
 		Id:      req.Data.Id,
