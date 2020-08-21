b7a7bad46501a9dca820e9db84c5b09e52358bac
Algys Ievlev <algys.ievlev@gmail.com>
2020-08-21 14:35:23 +0300

    [DPM-569] fix race and peform game action and newgame in same trx

--- server/api/handlers/new_game.go
+++ server/api/handlers/new_game.go
@@ -4,11 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"github.com/eoscanada/eos-go"
-	"github.com/rs/zerolog/log"
 	"platform-backend/contracts"
-	"platform-backend/models"
 	"platform-backend/server/api/ws_interface"
-	"time"
 )
 
 type NewGamePayload struct {
@@ -41,40 +38,18 @@ func ProcessNewGameRequest(context context.Context, req *ws_interface.ApiRequest
 		return nil, ws_interface.NewHandlerError(ws_interface.InternalError, err)
 	}
 
-	session, err := req.UseCases.GameSession.NewSession(
-		context, cas, game,
-		req.User, payload.Deposit,
-	)
-	if err != nil {
-		return nil, ws_interface.NewHandlerError(ws_interface.InternalError, err)
-	}
-
-	err = req.Repos.GameSession.AddGameSessionUpdate(context, &models.GameSessionUpdate{
-		SessionID:  session.ID,
-		UpdateType: models.SessionCreatedUpdate,
-		Timestamp:  time.Now(),
-		Data:       nil,
-	})
-	if err != nil {
-		return nil, ws_interface.NewHandlerError(ws_interface.InternalError, err)
-	}
-
 	actionParams := make([]uint64, len(payload.ActionParams))
 	for i, param := range payload.ActionParams {
 		actionParams[i] = uint64(param)
 	}
 
-	// try to instantly make first game action
-	err = req.UseCases.GameSession.GameAction(context, session.ID, payload.ActionType, actionParams)
-	if err != nil { // if error just save action to db
-		log.Debug().Msgf("Instant first action failed, saving action params for session: %d", session.ID)
-		err = req.Repos.GameSession.AddFirstGameAction(context, session.ID, &models.GameAction{
-			Type:   payload.ActionType,
-			Params: actionParams,
-		})
-		if err != nil {
-			return nil, ws_interface.NewHandlerError(ws_interface.InternalError, err)
-		}
+	session, err := req.UseCases.GameSession.NewSession(
+		context, cas, game,
+		req.User, payload.Deposit,
+		payload.ActionType, actionParams,
+	)
+	if err != nil {
+		return nil, ws_interface.NewHandlerError(ws_interface.InternalError, err)
 	}
 
 	return toGameSessionResponse(session), nil
