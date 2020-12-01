15bb4939a9ee3cac96bffc0d7a4114f304731e94
allerria <wtf496@gmail.com>
2020-12-01 16:37:13 +0300

    add index on deposit_value desc

--- migrations/0012_separate_bet_token_value.up.sql
+++ migrations/0012_separate_bet_token_value.up.sql
@@ -3,6 +3,8 @@ ADD COLUMN symbol VARCHAR(7) DEFAULT 'BET',
 ADD COLUMN deposit_value NUMERIC(13, 4) DEFAULT 0.0000,
 ADD COLUMN player_win_value NUMERIC(13, 4) DEFAULT NULL;
 
+CREATE INDEX sessions_deposits_idx ON game_sessions(deposit_value DESC);
+
 UPDATE game_sessions SET symbol = SUBSTRING(deposit, '[A-Z]+$')::VARCHAR(7);
 UPDATE game_sessions SET deposit_value = SUBSTRING(deposit, '^\d+.\d+')::NUMERIC(13, 4);
 UPDATE game_sessions SET player_win_value =
