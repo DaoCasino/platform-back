a2e5361141d8ed26a0ae9898a7a53e7eb2d143d4
georgiypetrov <petrovgeorgiy11@gmail.com>
2021-03-02 18:11:37 +0300

    add SignInTestAccount test

--- auth/usecase/usecase_test.go
+++ auth/usecase/usecase_test.go
@@ -250,3 +250,67 @@ func TestOptOut(t *testing.T) {
 	_, _, err = uc.SignUp(ctx, user, casinoName)
 	assert.NoError(t, err)
 }
+
+func TestSignInTestAccount(t *testing.T) {
+	repo := new(mock.UserStorageMock)
+	cbRepo := new(mock2.CashbackRepoMock)
+	sm := new(smMockRepo.MockRepository)
+	contractUC := new(usecase.ContractsUseCaseMock)
+
+	uc := NewAuthUseCase(
+		repo,
+		sm,
+		cbRepo,
+		contractUC,
+		[]byte("secret"),
+		10,
+		10,
+		"",
+		0,
+		"",
+		[]string{"daotestacc"},
+	)
+
+	var (
+		accountName = "daotestacc"
+		email       = "user@user.com"
+		suid, _     = uuid.NewRandom()
+		affiliateID = "affiliate_1"
+		casinoName  = "casinoxxxx"
+
+		ctx = context.Background()
+
+		user = &models.User{
+			AccountName: accountName,
+			Email:       email,
+			AffiliateID: affiliateID,
+		}
+		salt = uint64(123123)
+		hash = "cae9ba368c634dde9089fda1348e4ca2182f5e3ff82963e625905f3e3e688702"
+	)
+
+	tokenNonce := int64(0)
+	nextTokenNonce := int64(1)
+
+	// Sign Up (Get auth token)
+	repo.On("HasUser", user.AccountName).Return(false, nil)
+	repo.On("AddUser", user).Return(nil)
+	cbRepo.On("AddUser", user.AccountName).Return(nil)
+	contractUC.On("SendBonusToNewPlayer", user.AccountName, casinoName).Return(nil)
+	repo.On("HasEmail", user.AccountName).Return(true, nil)
+	repo.On("IsSessionActive", user.AccountName, tokenNonce).Return(true, nil)
+	repo.On("InvalidateSession", user.AccountName).Return(nil)
+	repo.On("AddNewSession", user.AccountName).Return(nextTokenNonce, nil)
+	_, _, err := uc.SignUp(ctx, user, casinoName)
+	assert.NoError(t, err)
+
+	// Auth with access token
+	ctx = context.WithValue(ctx, "suid", suid)
+	repo.On("GetUser", user.AccountName).Return(user, nil)
+	sm.On("SetUser", suid, user).Return(nil)
+	repo.On("GetTestAccountSalt").Return(salt)
+	repo.On("UpdateTestAccountSalt").Return()
+	parsedUser, err := uc.SignInTestAccount(ctx, accountName, hash)
+	assert.NoError(t, err)
+	assert.Equal(t, user, parsedUser)
+}
